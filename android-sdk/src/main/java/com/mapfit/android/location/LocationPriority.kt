2047355a8e6cd524a9dd2243dad90d194ebd2953
dogangulcan <dgngulcan@gmail.com>
2018-03-22 14:34:45 -0400

    remove medium accuracy

--- android-sdk/src/main/java/com/mapfit/android/location/LocationPriority.kt
+++ android-sdk/src/main/java/com/mapfit/android/location/LocationPriority.kt
@@ -8,18 +8,12 @@ enum class LocationPriority(private val priority: Int) {
      * Most accurate location will be obtained. It will consume more power. Expect an accuracy of
      * less than 100 meters.
      */
-    ACCURATE(3),
-
-    /**
-     * Balanced in terms of accuracy and power consumption. Expect an accuracy between 100 and 500
-     * meters.
-     */
-    BALANCED(2),
+    HIGH_ACCURACY(3),
 
     /**
      * Focuses on low power consumption. Expect an accuracy greater than 500 meters.
      */
-    LOW_POWER(1);
+    LOW_ACCURACY(1);
 
     internal fun getPriority(): Int {
         return priority
