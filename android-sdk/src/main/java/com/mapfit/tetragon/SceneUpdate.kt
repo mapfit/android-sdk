dd37cc2d1afe8578e5032c3c97662f3117ebcf80
dogangulcan <dgngulcan@gmail.com>
2018-05-28 17:04:22 -0400

    change documentation comment

--- android-sdk/src/main/java/com/mapfit/tetragon/SceneUpdate.kt
+++ android-sdk/src/main/java/com/mapfit/tetragon/SceneUpdate.kt
@@ -3,8 +3,8 @@ package com.mapfit.tetragon
 /**
  * Represents a data structure to specify a yaml path and the corresponding value.
  *
- * @param path  Series of yaml keys separated by a ".". Represents the scene path to be updated
- * @param value A yaml string which will update the value at the specified path
+ * @param path  path of the property to be changed. use dot(".") to declare scopes. e.g. "global.show_transit"
+ * @param value yaml string which will update the value at the specified path
  */
 data class SceneUpdate(val path: String, val value: String) {
 
