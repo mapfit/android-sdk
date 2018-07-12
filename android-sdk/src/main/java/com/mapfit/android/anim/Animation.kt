fc6a9738bd09b7e458494ad948207b01716e47b8
dogangulcan <dgngulcan@gmail.com>
2018-07-12 17:40:38 -0400

    add running and cancelled fields

--- android-sdk/src/main/java/com/mapfit/android/anim/Animation.kt
+++ android-sdk/src/main/java/com/mapfit/android/anim/Animation.kt
@@ -4,7 +4,9 @@ import android.graphics.drawable.Animatable
 
 abstract class Animation : Animatable {
 
+    var running = false
     var finished = false
     var paused = false
+    var cancelled = false
 
 }
