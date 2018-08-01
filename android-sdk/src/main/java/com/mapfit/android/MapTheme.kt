9b95ac48ce9bdade28ccc8e55de98e7a73f5cc03
dogangulcan <dgngulcan@gmail.com>
2018-08-01 16:57:19 -0400

    update Mapfit themes

--- android-sdk/src/main/java/com/mapfit/android/MapTheme.kt
+++ android-sdk/src/main/java/com/mapfit/android/MapTheme.kt
@@ -3,13 +3,11 @@ package com.mapfit.android
 
 /**
  * Built in Mapfit map themes.
- *
- * Created by dogangulcan on 12/21/17.
  */
 enum class MapTheme(private val scenePath: String) {
-    MAPFIT_DAY("https://cdn.mapfit.com/v2-4/themes/mapfit-day.yaml"),
-    MAPFIT_NIGHT("https://cdn.mapfit.com/v2-4/themes/mapfit-night.yaml"),
-    MAPFIT_GRAYSCALE("https://cdn.mapfit.com/v2-4/themes/mapfit-grayscale.yaml");
+    MAPFIT_DAY("https://cdn.mapfit.com/v3-0/themes/mapfit-day.yaml"),
+    MAPFIT_NIGHT("https://cdn.mapfit.com/v3-0/themes/mapfit-night.yaml"),
+    MAPFIT_GRAYSCALE("https://cdn.mapfit.com/v3-0/themes/mapfit-grayscale.yaml");
 
     override fun toString(): String {
         return scenePath
